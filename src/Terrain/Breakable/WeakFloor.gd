4d0dec523d50ac8daf7f16a0157510929d97a687
OrusLiOx <y.lily.n@gmail.com>
2024-08-11 10:53:36 -0500

    added falling platform animations + falling platform should now behave correctly if you change weight while on the platform

--- src/Terrain/Breakable/WeakFloor.gd
+++ src/Terrain/Breakable/WeakFloor.gd
@@ -7,10 +7,19 @@ var timer :Timer
 var playerDetector : CollisionShape2D
 var terrain : CollisionShape2D
 var sprite
-var respawnCol : CollisionShape2D
+var player
+
+var dying : bool
+var respawning : bool
+var shake:float
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	player = null
+	# c = (weightLimit+maxDark)/(maxDark+maxLight)
+	# modulate = Color(c,c,c)
+	shake = 0
+	size.x = max(size.x ,50)
 	timer = $Timer
 	sprite = $Polygon2D
 	
@@ -21,43 +30,80 @@ func _ready():
 	$Terrain/CollisionShape2D.shape.size.x = size.x
 	$Terrain.position.x = size.x/2
 	
+	$Area2D/CollisionShape2D.shape.size.x = size.x
+	$Area2D.position.x = size.x/2
 	
 	playerDetector = $PlayerDetector/CollisionShape2D
-	$PlayerDetector/CollisionShape2D.shape.size.x = size.x
+	$PlayerDetector/CollisionShape2D.shape.size.x = size.x-5
 	$PlayerDetector.position.x = size.x/2
 	
-	clip_children=1
+	self_modulate.a = 0
+	clip_children=0
 	pass # Replace with function body.
 
+func _process(delta):
+	if player != null:
+		if player.GetLightScale()<=weightLimit:
+			destroy()
+		
+	if respawning:
+		sprite.modulate.a += 4*delta
+		if sprite.modulate.a >=1:
+			respawning = false
+	elif dying:
+		sprite.modulate.a -= 4*delta
+		sprite.position.y += 400*delta;
+		if sprite.modulate.a <=0:
+			dying = false
+	elif shake > 0:
+		sprite.position.y += shake*delta*10
+		if sprite.position.y >= shake:
+			shake *= -1
+	elif shake <0:
+		sprite.position.y += shake*delta*10
+		if sprite.position.y <= 0:
+			sprite.position.y = 0
+			shake = 0
+
+# DESTROY STUFF
 func destroy():
-	terrain.disabled = true
-	playerDetector.disabled = true
-	sprite.visible = false
+	respawning = false
+	shake = 0
+	sprite.position.y = 0
+	terrain.set_deferred("disabled", true)
+	playerDetector.set_deferred("disabled", true)
+	dying = true
 	timer.start(respawnTime)
-	
-	# TODO: fling particles
-	
-func respawn():
-	respawnCol.disabled = false
-	terrain.disabled = false
-	playerDetector.disabled = false
-	sprite.visible = true
-	
-	# TODO: respawn animation
-	
+
 func _on_player_detector_body_entered(body):
+	player = body
 	if body.GetLightScale()<=weightLimit:
 		destroy()
+	else:
+		shake = max(6-body.GetLightScale()+weightLimit,1)
 	pass # Replace with function body.
+
+# RESPAWN STUFF
+func respawn():
+	sprite.position.y = 0;
+	respawning = true
+	shake = 0
+	sprite.position.y = 0
+	terrain.set_deferred("disabled", false)
+	playerDetector.set_deferred("disabled", false)
 	
 func _on_timer_timeout():
 	timer.stop()
 	if !$Area2D.has_overlapping_bodies():
 		respawn()
-	else:
-		respawnCol.disabled = true
 	pass # Replace with function body.
 
-func _on_area_2d_area_exited(area):
-	respawn()
+func _on_area_2d_body_exited(body):
+	if timer.is_stopped():
+		respawn()
+	pass # Replace with function body.
+
+
+func _on_player_detector_body_exited(body):
+	player = null
 	pass # Replace with function body.
